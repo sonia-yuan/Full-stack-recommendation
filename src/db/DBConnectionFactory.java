554144a1aec34f29d5f253b4d609e7ffee8a75de
yuansj <yuansj@yuansjdeMacBook-Pro.local>
2019-08-04 19:05:57 -0400

    login/logout

--- src/db/DBConnectionFactory.java
+++ src/db/DBConnectionFactory.java
@@ -5,7 +5,7 @@ import db.mysql.MySQLConnection;
 
 public class DBConnectionFactory {
 	// This should change based on the pipeline.
-		private static final String DEFAULT_DB = "mongodb";
+		private static final String DEFAULT_DB = "mysql";
 		
 		public static DBConnection getConnection(String db) {
 			switch (db) {
